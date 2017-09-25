190ab67a39434701c62fe19fdcaa27ae19b3d53a
panga kevin <pangakevin@gmail.com>
2017-09-25 20:12:49 +0200

    end all modif

--- resources/views/main/reservation.blade.php
+++ resources/views/main/reservation.blade.php
@@ -1,7 +1,7 @@
 @extends('main.layouts.app')
 
 @section('content')
-    <div class="fh5co-parallax" style="background-image: url(images/banner/banner4.jpg); height:300px;" data-stellar-background-ratio="0.5" style="height:300px;">
+    <div class="fh5co-parallax" style="background-image: url(images/banner/bann.jpg); height:300px;" data-stellar-background-ratio="0.5" style="height:300px;">
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table" style="height:300px;">
