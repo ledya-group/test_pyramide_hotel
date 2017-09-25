dae890a20ce187b8b417655a3c72a5a1abfd9dc5
panga kevin <pangakevin@gmail.com>
2017-09-25 20:08:35 +0200

    end modif

--- resources/views/main/contact.blade.php
+++ resources/views/main/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('main.layouts.app')
 
 @section('content')
-    <div class="fh5co-parallax" style="background-image: url(images/banner/banner4.jpg);font-size: small; height:300px;" data-stellar-background-ratio="0.5" style="height:300px;">
+    <div class="fh5co-parallax" style="background-image: url(images/logo/bann.jpg);font-size: small; height:300px;" data-stellar-background-ratio="0.5" style="height:300px;">
         <div class="overlay" style="height:300px;"></div>
         <div class="container">
             <div class="row">
@@ -16,24 +16,12 @@
 
     <div class="col-md-8 col-md-offset-2" style="padding-top:3em">
         <h3>Nos adresses</h3>
-
-        <p>
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.3640627728028!2d15.251169214760994!3d-4.342586696843714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a3197e1c63b0f%3A0xbd6fa66c5682a398!2sLedya+Pyramide+Hotel+Kinshasa!5e0!3m2!1sfr!2sfr!4v1502935598763" 
-                         width="100%" height="300" frameborder="0">
-                        </iframe>   
-                    </div>
-                </div>
-            </div>
-        </p>
         
         <ul class="contact-info">
             <li><i class="ti-map"></i> 35 Avenue Nguma , Macampagne, Kinshasa</li>
             <li><i class="ti-mobile"></i>+243 820005454 ; 82005464</li>
-            <li><i class="ti-envelope"></i><a href="#">info@pyramide-hotel.com</a></li>
-            <li><i class="ti-home"></i><a href="#">www.pyramide-hotel.com</a></li>
+            <li><i class="ti-envelope"></i><a href="mailto:info@pyramide-hotel.com">info@pyramide-hotel.com</a></li>
+            <li><i class="ti-home"></i><a href="www.pyramide-hotel.com">www.pyramide-hotel.com</a></li>
         </ul>
     </div>
 
@@ -60,5 +48,18 @@
                 </div>
             </div>
         </div>
-    </form>
+    
+    <p>
+            <div class="row">
+                <h3>Où sommes nous !</h3>
+                <div class="col-sm-12">
+                    <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.3640627728028!2d15.251169214760994!3d-4.342586696843714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a3197e1c63b0f%3A0xbd6fa66c5682a398!2sLedya+Pyramide+Hotel+Kinshasa!5e0!3m2!1sfr!2sfr!4v1502935598763" 
+                         width="100%" height="300" frameborder="0">
+                        </iframe>   
+                    </div>
+                </div>
+            </div>
+        </p>
+        </form>
 @endsection
