bc486d12f69be4dd5c733ff0bd20748e3ddcfcb2
Daniel RUBANGO <danielrubango@gmail.com>
2017-10-03 13:33:30 +0100

    Correction affichage nom du client dans le formulaire d'ajout d'une reservation

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -29,7 +29,7 @@
                         <option>Selectionner un client</option>
 
                         @foreach($clients as $client)
-                            <option value="{{ $client->id }}">{{ $client->profile->first_name . ' ' . $client->profile->first_name }}</option>
+                            <option value="{{ $client->id }}">{{ $client->present()->fullName() }}</option>
                         @endforeach
                     </select>
                 </div>
