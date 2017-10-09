f8fee63bdf7509663b173cb27d3a9378f90ca268
Daniel RUBANGO <danielrubango@gmail.com>
2017-10-09 17:30:26 +0100

    Final commit 09-10-2017

--- resources/views/main/partials/room_data.blade.php
+++ resources/views/main/partials/room_data.blade.php
@@ -12,7 +12,7 @@
 
         <p  style="text-align: justify;">
             La suite a tout ce qu’ une chambre présidentielle exige par son design élégant et mobilier raffiné qui sont les seuls mots de notre sublime suite présidentielle, également la plus spacieuse et la plus luxueuse. Dotée d’une chambre spacieuse, D’une superbe baignoire pouvant être transformée en Jacuzzi qui vous met dans une grande relaxation. Elle est dotée aussi d’un grand lit « King » paré de linge en coton et elle abrite également une cuisine classe qui vous permet de faire ce que vous désiriez manger, un salon qui qui vous permet de rester avec vous visiteurs tout en gardant vos intimes personnel, un bureau qui vous permet de travailler dans une grande tranquillité.
-            <h4>Les Services disponibles</h4>    
+            <h4>Les Services disponibles</h4>
         <ul>
             <li>Connexion Wi-Fi Gratuite avec haut débit.</li>
             <li>Accès salle de sport</li>
@@ -43,19 +43,19 @@
 
         <p  style="text-align: justify;">
             Une chambre très confortable avec une décoration soignée et chaleureuse , la chambre delux est , comme son nom l'indique, une très belle chambre luxueuse pouvant contenir deux personnes dans un grand confort et modernité. Cette chambre saura faire de votre séjour un réel plaisir.
-            <h4>Les Services disponibles</h4>    
+            <h4>Les Services disponibles</h4>
         <ul>
             <li>Connexion Wi-Fi Gratuite.</li>
             <li>Accès salle de sport</li>
             <li>Accès à la piscine</li>
-            <li>Un Petit Déjeuner continental</li> 
+            <li>Un Petit Déjeuner continental</li>
         </ul>
-      
+
 
         </p>
 
         <p>
-            <a href="/reservation?room=4" class="btn btn-primary btn-luxe-primary">
+            <a href="/reservation?room_id=4" class="btn btn-primary btn-luxe-primary">
                 Réserver <i class="ti-angle-right"></i>
             </a>
         </p>
@@ -74,10 +74,10 @@
 
     <div class="desc" id="junior">
         <h3>Junior</h3>
-        
+
         <p  style="text-align: justify;">
             Une Suite performante et bien disposée avec un espace spacieux et particulier accueillant, la Suite Junior offre une élégance dans son sein muni d’une propreté remarquable. Elle a un salon qui vous me à l’aise ne pouvant pas donné un accès direct à la chambre.
-            <h4>Les Services disponibles</h4>    
+            <h4>Les Services disponibles</h4>
         <ul>
             <li>Connexion Wi-Fi Gratuite.</li>
             <li>Accès salle de sport</li>
@@ -85,9 +85,9 @@
             <li>Un Petit Déjeuner continental</li>
         </ul>
         </p>
-        
+
         <p>
-            <a href="/reservation?room=2" class="btn btn-primary btn-luxe-primary">
+            <a href="/reservation?room_id=2" class="btn btn-primary btn-luxe-primary">
                 Réserver <i class="ti-angle-right"></i>
             </a>
         </p>
@@ -109,7 +109,7 @@
 
         <p  style="text-align: justify;">
             Une chambre de qualité muni des équipements complets qu’a besoin une chambre digne de son rang vous offrant un large espace de confort et de raffinement, et vous assure une magnifique expérience.
-            <h4>Les Services disponibles</h4>    
+            <h4>Les Services disponibles</h4>
         <ul>
           <li>Connexion Wi-Fi Gratuite.</li>
           <li>Accès salle de sport</li>
@@ -119,7 +119,7 @@
         </p>
 
         <p>
-            <a href="/reservation?room=3" class="btn btn-primary btn-luxe-primary">
+            <a href="/reservation?room_id=3" class="btn btn-primary btn-luxe-primary">
                 Réserver <i class="ti-angle-right"></i>
             </a>
         </p>
@@ -138,10 +138,10 @@
 
     <div class="desc" id="standard">
         <h3>Standard</h3>
-        
+
         <p  style="text-align: justify;">
             Une chambre qui répond à vos besoins avec dans son sein des équipement moderne, elle vous offrent un accès Internet haut débit sans fil gratuit et, au choix, un grand lit pouvant contenir deux personnes.
-                <h4>Les Services disponibles </h4>    
+                <h4>Les Services disponibles </h4>
             <ul>
                 <li>Connexion Wi-Fi Gratuite.</li>
                 <li>Accès salle de sport</li>
@@ -149,9 +149,9 @@
                 <li>Un Petit Déjeuner continental</li>
             </ul>
         </p>
-        
+
         <p>
-            <a href="/reservation?room=5" class="btn btn-primary btn-luxe-primary">
+            <a href="/reservation?room_id=5" class="btn btn-primary btn-luxe-primary">
                 Réserver <i class="ti-angle-right"></i>
             </a>
         </p>
