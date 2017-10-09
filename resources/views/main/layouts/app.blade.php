229ceb54ca6b67ec864b2491e91430acb255bc5c
Daniel RUBANGO <danielrubango@gmail.com>
2017-10-09 17:03:02 +0100

    Google Tag On Client Side App

--- resources/views/main/layouts/app.blade.php
+++ resources/views/main/layouts/app.blade.php
@@ -66,6 +66,16 @@
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
 
+	<!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107785044-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-107785044-1');
+    </script>
+
 </head>
 <body>
 
